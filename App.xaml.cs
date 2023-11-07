7fd7451c4b049fda0906b8a2fc46cd361dcbe6ad
Alesha Auckram <85319825+AleshaAuckram@users.noreply.github.com>
2023-11-07 17:45:44 +1300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿	using TaskApp.Pattern.Views;
+using TaskApp.Pattern.ViewModels;
+namespace TaskApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new NavigationPage(new MainView());
+		BindingContext = new MainPageViewModel();
+    }
+}
